004cba779cd57802b19308cb342004f8974a9705
Brian Esquivel <brian.esquive@gmail.com>
2023-10-19 19:52:06 -0600

    Práctica #7 Branch creada

---
+++ Practica7.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from tabulate import tabulate
+import matplotlib.pyplot as plt
+from typing import Tuple, Dict, List
+import numpy as np
+from functools import reduce
+from scipy.stats import mode
+
+
+
