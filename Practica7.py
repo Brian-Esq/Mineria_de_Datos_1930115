46397de3239851fff85d1db288dfa8a95a888b8d
Brian Esquivel <brian.esquive@gmail.com>
2023-10-25 01:09:48 -0600

    Práctica #7 Agregados 3 scatters más teniendo como labels los bimestres

--- Practica7.py
+++ Practica7.py
@@ -4,10 +4,12 @@ from typing import Tuple, List
 import numpy as np
 
 
+# Coloreado de los puntos para los diferentes grupos
 def get_cmap(n, name="hsv"):
     return plt.cm.get_cmap(name, n)
 
 
+# Genera el scatter para tanto los grupos por bimestre e índice como sólo por el índice
 def scatter_group_by(
     file_path: str, df: pd.DataFrame, x_column: str, y_column: str, label_column: str
 ):
@@ -22,10 +24,27 @@ def scatter_group_by(
     plt.close()
 
 
+# Genera el scatter para los grupos por bimestre
+def scatter_group_by_bim(
+    file_path: str, df: pd.DataFrame, x_column: str, y_column: str, label_column: str
+):
+    fig, ax = plt.subplots()
+    labels = pd.unique(df[label_column])
+    cmap = get_cmap(len(labels) + 1)
+    for i, label in enumerate(labels):
+        filter_df = df.query(f"{label_column} == {label}")
+        ax.scatter(filter_df[x_column], filter_df[y_column], label=label, color=cmap(i))
+    ax.legend()
+    plt.savefig(file_path)
+    plt.close()
+
+
+# Obtiene la distancia euclidiana para el algoritmo de los k vecinos
 def euclidean_distance(p_1: np.array, p_2: np.array) -> float:
     return np.sqrt(np.sum((p_2 - p_1) ** 2))
 
 
+# Obtiene los k vecinos siendo 5 como valor para luego imprimir los valores en consola por cada scatter distinto
 def k_nearest_neightbors(
     points: List[np.array], labels: np.array, input_data: List[np.array], k: int
 ):
@@ -44,12 +63,15 @@ def k_nearest_neightbors(
     return predicted_labels
 
 
+# Comienzo de la lectura del CSV y otro agrupado para obtener el primer scatter
 df = pd.read_csv("P2ConsumoAgua2019.csv")
 df_mean = df.groupby(['bimestre', 'indice_des'])['consumo_total'].mean().reset_index()
 
+# Se usan en los k vecinos para que no haya problema con los labels del scatter
 label_to_number = {label: i for i, label in enumerate(df_mean['indice_des'].unique())}
 number_to_label = {i: label for i, label in enumerate(df_mean['indice_des'].unique())}
 
+#Primera llamada al scatter mandando como eje x el consumo total, el y como bimestre, y los labels que sean las descripciones de los índices
 scatter_group_by("scatterP7/grupos.png", df_mean, "consumo_total", "bimestre", "indice_des")
 list_t = [
     (np.array(tuples[0:1]), tuples[2])
@@ -63,9 +85,11 @@ kn = k_nearest_neightbors(
 )
 print(kn)
 
+# Igual que arriba pero con distinto dataframe usado
 label_to_number = {label: i for i, label in enumerate(df['indice_des'].unique())}
 number_to_label = {i: label for i, label in enumerate(df['indice_des'].unique())}
 
+#Llamada al scatter mandando como eje x el consumo total doméstico, el y como consumo total, y los labels que sean las descripciones de los índices
 scatter_group_by("scatterP7/gruposDom.png", df, "consumo_total_dom", "consumo_total", "indice_des")
 list_t = [
     (np.array(tuples[0:1]), tuples[2])
@@ -79,6 +103,7 @@ kn = k_nearest_neightbors(
 )
 print(kn)
 
+#Llamada al scatter mandando como eje x el consumo total no doméstico, el y como consumo total, y los labels que sean las descripciones de los índices
 scatter_group_by("scatterP7/gruposNoDom.png", df, "consumo_total_no_dom", "consumo_total", "indice_des")
 list_t = [
     (np.array(tuples[0:1]), tuples[2])
@@ -92,6 +117,7 @@ kn = k_nearest_neightbors(
 )
 print(kn)
 
+#Llamada al scatter mandando como eje x el consumo total mixto, el y como consumo total, y los labels que sean las descripciones de los índices
 scatter_group_by("scatterP7/gruposMixto.png", df, "consumo_total_mixto", "consumo_total", "indice_des")
 list_t = [
     (np.array(tuples[0:1]), tuples[2])
@@ -100,6 +126,54 @@ list_t = [
 kn = k_nearest_neightbors(
     df[['consumo_total_mixto','consumo_total']].to_numpy(),
     df['indice_des'].to_numpy(),
+    [np.array([100, 150]), np.array([1, 1]), np.array([1, 300]), np.array([80, 40])],
+    5
+)
+print(kn)
+
+# Se organiza el dataframe para que los bimestres tengan un orden ascendente
+df = df.sort_values(by='bimestre', ascending=True)
+# Ahora estos ayudarán para los k vecinos pero ahora la columna que será el label va a ser la de los bimestres
+label_to_number = {label: i for i, label in enumerate(df['bimestre'].unique())}
+number_to_label = {i: label for i, label in enumerate(df['bimestre'].unique())}
+
+#Llamada al scatter mandando como eje x el consumo total doméstico, el y como consumo total, y los labels que sean los 3 distintos bimestres
+scatter_group_by_bim("scatterP7/gruposBimDom.png", df, "consumo_total_dom", "consumo_total", "bimestre")
+list_t = [
+    (np.array(tuples[0:1]), tuples[2])
+    for tuples in df.itertuples(index=False, name=None)
+]
+kn = k_nearest_neightbors(
+    df[['consumo_total_dom', 'consumo_total']].to_numpy(),
+    df['bimestre'].to_numpy(),
+    [np.array([100, 150]), np.array([1, 1]), np.array([1, 300]), np.array([80, 40])],
+    5
+)
+print(kn)
+
+#Llamada al scatter mandando como eje x el consumo total no doméstico, el y como consumo total, y los labels que sean los 3 distintos bimestres
+scatter_group_by_bim("scatterP7/gruposBimNoDom.png", df, "consumo_total_no_dom", "consumo_total", "bimestre")
+list_t = [
+    (np.array(tuples[0:1]), tuples[2])
+    for tuples in df.itertuples(index=False, name=None)
+]
+kn = k_nearest_neightbors(
+    df[['consumo_total_no_dom', 'consumo_total']].to_numpy(),
+    df['bimestre'].to_numpy(),
+    [np.array([100, 150]), np.array([1, 1]), np.array([1, 300]), np.array([80, 40])],
+    5
+)
+print(kn)
+
+#Llamada al scatter mandando como eje x el consumo total mixto, el y como consumo total, y los labels que sean los 3 distintos bimestres
+scatter_group_by_bim("scatterP7/gruposBimMixto.png", df, "consumo_total_mixto", "consumo_total", "bimestre")
+list_t = [
+    (np.array(tuples[0:1]), tuples[2])
+    for tuples in df.itertuples(index=False, name=None)
+]
+kn = k_nearest_neightbors(
+    df[['consumo_total_mixto', 'consumo_total']].to_numpy(),
+    df['bimestre'].to_numpy(),
     [np.array([100, 150]), np.array([1, 1]), np.array([1, 300]), np.array([80, 40])],
     5
 )
