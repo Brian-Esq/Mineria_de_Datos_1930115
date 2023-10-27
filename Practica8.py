752b3e5cea26241565f7f513ec4f3aa10cb9c5a6
Brian Esquivel <brian.esquive@gmail.com>
2023-10-26 23:28:05 -0600

    Práctica #8 Primer commit

---
+++ Practica8.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from typing import Tuple, List
+import numpy as np
+
+
+# Coloreado de los puntos para los diferentes grupos
+def get_cmap(n, name="hsv"):
+    return plt.cm.get_cmap(name, n)
+
+
+
