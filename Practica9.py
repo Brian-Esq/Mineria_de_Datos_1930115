9ed6eb555f45e2fc6fc873f6d069d3591b544349
Brian Esquivel <brian.esquive@gmail.com>
2023-11-02 22:52:41 -0600

    Práctica #9 Primer commit de nueva branch

---
+++ Practica9.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+import numbers
+import pandas as pd
+from tabulate import tabulate
+from statsmodels.stats.outliers_influence import summary_table
+from typing import Tuple, Dict
+import numpy as np
+
+
