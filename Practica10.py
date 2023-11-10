3fbccd30d927ee33f85dcf15b173c958f34c40e3
Brian Esquivel <brian.esquive@gmail.com>
2023-11-10 14:29:37 -0600

    Práctica #10 Generadas mas nubes de palabras con los CSV

--- Practica10.py
+++ Practica10.py
@@ -1,6 +1,7 @@
 from wordcloud import WordCloud
 from collections import Counter
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def open_file(path: str) -> str:
@@ -8,6 +9,17 @@ def open_file(path: str) -> str:
     with open(path, "r") as f:
         content = f.readlines()
     return " ".join(content)
+
+
+def open_dataframe_column(df: pd.DataFrame, column_name: str) -> str:
+    # Verificar si la columna existe en el DataFrame
+    if column_name not in df.columns:
+        raise ValueError(f"La columna '{column_name}' no existe en el DataFrame.")
+
+    # Obtener el contenido de la columna como una cadena
+    content = " ".join(df[column_name].astype(str))
+
+    return content
 
 
 all_words = ""
@@ -33,4 +45,31 @@ plt.tight_layout(pad=0)
 
 # plt.show()
 plt.savefig("textCloudP10/palabrasTXT.png")
+plt.close()
+
+
+df = pd.read_csv("P2ConsumoAgua2019.csv")
+palabras = open_dataframe_column(df, 'colonia')
+palabras = " ".join(palabras.split())
+wordcloud2 = WordCloud(background_color="white", min_font_size=5).generate(palabras)
+plt.close()
+plt.figure(figsize=(5, 5), facecolor=None)
+plt.imshow(wordcloud2)
+plt.axis("off")
+plt.tight_layout(pad=0)
+
+plt.savefig("textCloudP10/palabrasColoniaCSV.png")
+plt.close()
+
+
+palabras2 = open_dataframe_column(df, 'alcaldia')
+palabras2 = " ".join(palabras2.split())
+wordcloud3 = WordCloud(background_color="white", min_font_size=5).generate(palabras2)
+plt.close()
+plt.figure(figsize=(5, 5), facecolor=None)
+plt.imshow(wordcloud3)
+plt.axis("off")
+plt.tight_layout(pad=0)
+
+plt.savefig("textCloudP10/palabrasAlcaldiaCSV.png")
 plt.close()
